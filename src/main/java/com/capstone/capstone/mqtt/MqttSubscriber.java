a71c1dff3f175f4b067238d75293d1fe4ba00cc8
dawon <da5720@naver.com>
2026-04-01 17:18:57 +0900

    4/1

--- src/main/java/com/capstone/capstone/mqtt/MqttSubscriber.java
+++ src/main/java/com/capstone/capstone/mqtt/MqttSubscriber.java
@@ -3,6 +3,4 @@ package com.capstone.capstone.mqtt;
 import tools.jackson.databind.ObjectMapper;
 
 public class MqttSubscriber {
-    private final ObjectMapper objectMapper;
-    private final DataProcessingService service;
 }
