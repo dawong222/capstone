7a667134175179177e5b8115b13538491b4a6687
dawon <da5720@naver.com>
2026-04-01 17:17:55 +0900

    4/1

---
+++ src/main/java/com/capstone/capstone/dto/ConstraintsDto.java
@@ -0,0 +1,13 @@
+package com.capstone.capstone.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConstraintsDto {
+    private double socMin;
+    private double socMax;
+    private double essMaxCharge;
+    private double essMacDischarge;
+}
