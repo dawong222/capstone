5fcdedd73d54b4e430e9c05b14f20d1ecd3d3d04
JosephHsm <jossep0218@sju.ac.kr>
2026-05-11 15:21:42 +0900

    mqtt받는 오류

--- src/main/java/com/capstone/capstone/dto/mqtt/MqttPayloadDto.java
+++ src/main/java/com/capstone/capstone/dto/mqtt/MqttPayloadDto.java
@@ -1,10 +1,12 @@
 package com.capstone.capstone.dto.mqtt;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
 
 @Getter @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MqttPayloadDto {
     private List<MqttStationDto> stations;
 }
